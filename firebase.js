96a845c062746da7df7fe79168d3aae9840289be
nicholaiitutor-bit <nicholaiitutor@gmail.com>
2026-06-01 11:59:03 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAs5VqodQCgH-F-VYbM1zS2BzsoHOQGpzo",
+  authDomain: "remote-work-hub-211d8.firebaseapp.com",
+  projectId: "remote-work-hub-211d8",
+  storageBucket: "remote-work-hub-211d8.firebasestorage.app",
+  messagingSenderId: "697750475671",
+  appId: "1:697750475671:web:6e609411ae003e6500aad2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
